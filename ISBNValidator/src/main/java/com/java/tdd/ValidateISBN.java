3416f96af0e913a887e2192db137c00ce2c8c583
haitam-elgharras <elgharras.haitam@gmail.com>
2024-08-18 19:53:47 +0100

    Refactor code with confidence when following TDD

--- ISBNValidator/src/main/java/com/java/tdd/ValidateISBN.java
+++ ISBNValidator/src/main/java/com/java/tdd/ValidateISBN.java
@@ -1,32 +1,45 @@
 package com.java.tdd;
 
 public class ValidateISBN {
+
+    private static final int LONG_ISBN = 13;
+    private static final int SHORT_ISBN = 10;
+    private static final int LONG_ISBN_MULTIPLIER = 10;
+    public static final int SHORT_ISBN_MULTIPLIER = 11;
+
     public boolean checkISBN(String ISBN) {
-        int total = 0;
+        if (ISBN.length()== LONG_ISBN)
+            return isAValid13DigitsISBN(ISBN);
 
-        if (ISBN.length()==13) {
-            for (int i =0; i < 13; i++) {
-                if (i % 2 == 0) total+=Character.getNumericValue(ISBN.charAt(i));
-                else total+=Character.getNumericValue(ISBN.charAt(i))*3;
-            }
-            return total % 10 == 0;
-        }
-
-        if (ISBN.length() != 10)
+        if (ISBN.length() != SHORT_ISBN)
             throw new NumberFormatException("An ISBN must be 10 digits long!");
 
-        for (int i = 0; i < 10; i++) {
+        return isAValid10DigitsISBN(ISBN);
+    }
+
+    private static boolean isAValid10DigitsISBN(String ISBN) {
+        int total=0;
+        for (int i = 0; i < SHORT_ISBN; i++) {
             if(!Character.isDigit(ISBN.charAt(i)) && (i != 9 || ISBN.charAt(i) != 'X' ))
                 throw new NumberFormatException("The ISBN must be a valid numeric value!");
 
             if (i == 9 && ISBN.charAt(i) == 'X') {
-                total += 10;
+                total += SHORT_ISBN;
                 break;
             }
-
-            total += Character.getNumericValue(ISBN.charAt(i)) * (10 - i);
+            total += Character.getNumericValue(ISBN.charAt(i)) * (SHORT_ISBN - i);
         }
-        return total % 11 == 0;
+        return total % SHORT_ISBN_MULTIPLIER == 0;
+    }
+
+    private boolean isAValid13DigitsISBN(String ISBN) {
+        int total = 0;
+
+        for (int i = 0; i < LONG_ISBN; i++) {
+            if (i % 2 == 0) total +=Character.getNumericValue(ISBN.charAt(i));
+            else total +=Character.getNumericValue(ISBN.charAt(i))*3;
+        }
+        return total % LONG_ISBN_MULTIPLIER == 0;
     }
 
 }
